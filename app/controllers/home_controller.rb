9d9266427440f49bb2439d1cb76e9ce2e2947aeb
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-08 00:03:11 +0900

    static_page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def contact
+  end
+
+  def about
+  end
+  
+  def help
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'home#index'
+  get '/about',   to: 'home#about'
+  get '/contact', to: 'home#contact'
+  get '/help',    to: 'home#help'
 end
