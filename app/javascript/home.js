7ed4444979736ea91e1e735236dc0b630da43f03
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-13 23:04:46 +0900

    home2

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -1,6 +1,7 @@
 
   $(function() {
-    $(".imagefadeout").fadeOut(2000);
+    $(".imagefadeout").fadeIn(1500);
+    $(".imagefadeout").fadeOut(1500);
     $(".main-title").fadeIn(6000);
     $(".main-title2").slideDown(8000);
     $(".main-title-p").fadeIn(16000);
