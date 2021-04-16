2dcb5453a35cccd41b97f62d219b997e8d248b9b
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-16 12:23:48 +0900

    play1

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -14,3 +14,48 @@ $(function () {
   });
 
 });
+
+$(function () {
+  $('.hide-btn2').click(function () {
+    // $('#hide-title2')を変数$titleに代入してください
+    var $title = $('#hide-title2');
+
+
+    // 「#title」に対する3つのメソッドを、変数を使って書き換えてください
+    $title.css('color', 'red');
+    $title.html('みなさん、こんばんは');
+    $title.fadeOut(1000);
+
+    // 「#text」に対する3つのメソッドを、メソッドチェーンを使って書き換えてください
+    $('#hide-text2').css('color', 'blue').html('<h3>jQueryをマスターしましょう！</h3>').fadeOut(1000);
+  });
+});
+
+$(function () {
+  $('#find-method').click(function () {
+    // findメソッドで、「#wrapper」内にあるすべての「a」要素を取得し、
+    // cssメソッドで文字の色をredに指定してください
+    $('#hide-wrapper').find('a').css('color', 'red');
+
+
+  });
+
+  $('#children-method').click(function () {
+    // childrenメソッドで、「#wrapper」の一階層下にある「a」要素を取得し、
+    // fadeOutメソッドで隠してください
+    $('#hide-wrapper').children('a').fadeOut();
+
+
+  });
+});
+
+$(function () {
+  // 「#language-wrapper」にhoverしたときのhoverイベントを作成してください
+  $('#language-wrapper').hover(function () {
+    $('.language-text').fadeIn();
+  },
+    function () {
+      $('.language-text').fadeOut();
+    });
+
+});
