f03fdffa67fcde8ad06d09a728a21a9bc3ab24c3
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-18 01:02:03 +0900

    finish-home3

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -71,3 +71,31 @@ $(function() {
   });
 });
 
+$(function(){
+  $('.icon-hover').hover(
+    function(){
+      $(this).find('.ruby-icon-p').addClass('text-active');
+    },
+    function(){
+      $(this).find('.ruby-icon-p').removeClass('text-active');
+    });
+
+  $('.faq-list-item').click(function () {
+    var $answer = $(this).find('.answer');
+    if ($answer.hasClass('open')) {
+      $answer.removeClass('open');
+      // slideUpメソッドを用いて、$answerを隠してください
+      $answer.slideUp();
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('+');
+
+    } else {
+      $answer.addClass('open');
+      // slideDownメソッドを用いて、$answerを表示してください
+      $answer.slideDown();
+
+      // 子要素のspanタグの中身をtextメソッドを用いて書き換えてください
+      $(this).find('span').text('-');
+    }
+  });
+});
