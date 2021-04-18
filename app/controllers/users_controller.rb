3d6edbe37a266027d1f379bea4d30093c38d2f10
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-18 21:29:17 +0900

    new create action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    redirect_to root_url
+    else
+      render 'new'
+    end
+
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   root to: 'home#index'
   get '/about',   to: 'home#about'
   get '/practice', to: 'home#practice'
   get '/help',    to: 'home#help'
+  resources :users
 end
