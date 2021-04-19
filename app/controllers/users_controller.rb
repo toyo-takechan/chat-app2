0aa4961b3378d18042bca9ef6c0ca50ce5b8fa6e
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-20 00:39:01 +0900

    finishuser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
 end
