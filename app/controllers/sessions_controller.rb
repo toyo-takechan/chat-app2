98993e0223736552ae29aa7ce3f349018000f531
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-22 11:19:20 +0900

    advanced-login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      # if params[:session][:remember_me] == '1'
+      #   remember(user)
+      # else
+      #   forget(user)
+      # end
       redirect_to user
     else
       flash.now[:danger] = "メールアドレスとパスワードの組み合わせが無効です"
@@ -14,7 +20,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
