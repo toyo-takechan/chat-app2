53bd47cf415e0e45ef529da16bf559dc9b227b9f
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-22 11:45:34 +0900

    forwarding_url

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
       # else
       #   forget(user)
       # end
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:danger] = "メールアドレスとパスワードの組み合わせが無効です"
       render 'new'
