73b4b14ec5fb70181d236f43338a04abf4b5b72b
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-26 00:55:24 +0900

    gazo

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :destroy]
+  before_action :correct_user,   only: :destroy
   
   def new
     @post = Post.new
@@ -7,6 +8,7 @@ class PostsController < ApplicationController
   
   def create
     @post = current_user.posts.build(post_params)
+    @post.image.attach(params[:post][:image])
     if @post.save
       flash[:info] = "投稿に成功しました。"
       redirect_to user_path(current_user)   
@@ -16,11 +18,19 @@ class PostsController < ApplicationController
   end
 
   def destroy
+    @post.destroy
+    flash[:success] = "投稿を削除ししました。"
+    redirect_to request.referrer || root_url
   end
 
   private
 
   def post_params
     params.require(:post).permit(:content, :image )
+  end
+
+  def correct_user
+    @post = current_user.posts.find_by(id: params[:id])
+    redirect_to root_url if @post.nil?
   end
 end
