c705c6be2095d3dbcddafc69e923cc88a7d92e98
toyo-takechan <42319201+toyo-takechan@users.noreply.github.com>
2021-04-25 16:26:59 +0900

    post new create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,26 @@
 class PostsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :destroy]
   
+  def new
+    @post = Post.new
+  end
+  
   def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      flash[:info] = "投稿に成功しました。"
+      redirect_to user_path(current_user)   
+    else
+     render 'new' 
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content, :image )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
-  resources :posts,          only: [:new, :create, :destroy]
+  resources :posts
 end
